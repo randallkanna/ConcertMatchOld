e6c17823eed6578ec65002da8332fd1483617ff4
randallkanna <randallkanna@gmail.com>
2015-02-02 20:10:24 -0800

    working with fake data to start the design

--- app/assets/javascripts/routes/activities_route.js
+++ app/assets/javascripts/routes/activities_route.js
@@ -1,9 +1,259 @@
 Concertmatch.ActivitiesRoute = Ember.Route.extend({
   model: function() {
     return [{
+      title: "Rockin Twenties",
       venue: "Ace of Spades",
-      city: "Sacramento"
-    }];
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+    {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+    {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+    {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+    {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+    {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+    {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+        {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+            {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    },
+    {
+      title: "Rockin Twenties",
+      venue: "Ace of Spades",
+      city: "Sacramento",
+      state: "CA",
+      zipcode: "95819"
+    }
+
+    ];
   }
 });
 
--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -20,5 +20,5 @@ end
 
 
   # def index
-  #   respond_with Book.all
+  #   respond_with Concert.all
   # end
