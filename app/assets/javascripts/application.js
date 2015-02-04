85189da3559b35540cf2ddbf41ab1c49222d34f7
randallkanna <randallkanna@gmail.com>
2015-02-03 18:25:14 -0800

    working on fixing the activities route and passing the correct api info

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,24 @@
 // for more details see: http://emberjs.com/guides/application/
 Concertmatch = Ember.Application.create();
 
+Concertmatch.Activity = Ember.Object.extend();
+Concertmatch.Activity.reopenClass({
+  allActivities: [],
+  all: function(){
+    this.allActivities = [];
+    $.ajax({
+      url: 'http://api.jambase.com/events?zipCode=95128&page=0&api_key==rct6rka5zncmwnvz38srk36y',
+      dataType: 'jsonp',
+      context: this,
+      success: function(response){
+        response.data.forEach(function(activity){
+          this.allActivities.addObject(Concertmatch.Activity.create(activity))
+        }, this)
+      }
+    })
+    return this.allActivities;
+  }
+});
+
+
+
--- app/assets/javascripts/routes/activities_route.js
+++ app/assets/javascripts/routes/activities_route.js
@@ -1,267 +1,269 @@
 Concertmatch.ActivitiesRoute = Ember.Route.extend({
   model: function() {
-    return [{
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-    {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-    {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-    {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-    {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-    {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-    {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-        {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-            {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    },
-    {
-      title: "Rockin Twenties",
-      venue: "Ace of Spades",
-      city: "Sacramento",
-      state: "CA",
-      zipcode: "95819"
-    }
-
-    ];
+    return Concertmatch.Activity.all();
   }
 });
 
-// Concertmatch.ActivitiesRoute = Ember.Route.extend({
-//   // model: function() {
-//   //   return Concertmatch.Activity.find();
-//   // }
-// });
+
+// return this.store.find('activity');
 
 
-// return this.get('store').find('activities');
+
+// // Fake data:
+
+ // model: function() {
+ //    return [{
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //    {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //    {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //    {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //    {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //    {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //    {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //        {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //            {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    },
+ //    {
+ //      title: "Rockin Twenties",
+ //      venue: "Ace of Spades",
+ //      city: "Sacramento",
+ //      state: "CA",
+ //      zipcode: "95819"
+ //    }
+
+ //    ];
+ //  }
